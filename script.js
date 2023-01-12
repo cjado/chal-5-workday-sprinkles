34b0840794cb7f41dd06b3cc4111816fb2397a74
Cj adorna <cjadorna@hotmail.com>
2023-01-12 15:15:07 -0600

    time for past presetn and future classes working// save button works ----- not with refresh

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+    
+
+    $(document).on('click', '.saveBtn', function() {
+        var $this = $(this);
+        var hour = parseInt($this.closest('.time-block').attr('id').split('-')[1]);
+        var description = $this.prev().val();
+        localStorage.setItem('hour-' + hour, description);
+    })
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -9,11 +18,23 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+
+    $('.time-block').each(function() {
+        var now = dayjs();
+        var hour = now.hour();
+        var $this = $(this);
+        var id = parseInt($this.attr('id').split('-')[1]);
+
+        if (hour > id) {
+            $this.addClass('past');
+            $this.removeClass('present future');
+        } else if (hour < id) {
+            $this.addClass('future');
+            $this.removeClass('present past');
+        } else {
+            $this.addClass('present');
+            $this.removeClass('future past');
+        }});
     //
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
@@ -21,3 +42,4 @@ $(function () {
     //
     // TODO: Add code to display the current date in the header of the page.
   });
+
